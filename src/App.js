366fdabd0bdb39bc608fa99bd68b95f146785551
Anika Knyshenko <anika.kn.902@gmail.com>
2025-01-13 10:44:14 +0200

    code update

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog version1
+            EKS DEMO Blog version2
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
